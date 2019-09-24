427a61ab2291319574fc57d0bf974a85ebe710ca
Rohit Shelar <rohit.shelar@riomed.com>
2019-09-24 19:15:17 +0530

    space removed

--- Day1.2-without Main/learn/statics/B.java
+++ Day1.2-without Main/learn/statics/B.java
@@ -3,14 +3,13 @@ package learn.statics;
 public class B extends A {
 
 	
-	protected static void m1() {
+	private static void m1() {
 		System.out.println("m1===B");
 			}
 
 	@Override
 	public void m2() {
 		System.out.println("m2===Bbbbb");		
-	
 	}
 
 
